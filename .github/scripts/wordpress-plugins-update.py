c4854fc561abe9b7239062aacd507f9df8db6211
y0no <y.onoditbiot@attineos-cyber.com>
2024-06-18 17:23:08 +0200

    Remove trailing slash from URL

--- .github/scripts/wordpress-plugins-update.py
+++ .github/scripts/wordpress-plugins-update.py
@@ -30,7 +30,7 @@ ranking = 1
 # Top 200 Wordpress Plugins
 for page_number in range(1, 11):
 
-    html = requests.get(url=f"https://wordpress.org/plugins/browse/popular/page/{page_number}/", headers={
+    html = requests.get(url=f"https://wordpress.org/plugins/browse/popular/page/{page_number}", headers={
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
         "Accept-Language": "en-US,en;q=0.9",
         "Accept-Encoding": "gzip, deflate",
